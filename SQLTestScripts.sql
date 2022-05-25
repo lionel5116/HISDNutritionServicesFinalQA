c0677ac540f716e5ce1034f003ca4ba1ca496ade
p00149021@houstonisd.org Mag17615@7 <lioneljones5116@gmail.com>
2022-05-25 11:02:34 -0500

    School Year Fix

---
+++ SQLTestScripts.sql
@@ -0,0 +1,36 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Year]
+  FROM [HISDNutritionalServices].[dbo].[SchoolYears]
+  order by Year desc
+
+  DELETE 
+  SchoolYears
+  WHERE 
+  Year = '2020-2021'
+
+
+  
+INSERT INTO SchoolYears (Year) VALUES ('2020-2021')
+
+SELECT  max(a.[Year]) as Year
+FROM SchoolYears a
+
+
+/*
+SELECT TOP (1000) [LastName]
+      ,[FirstName]
+	  ,School
+	  ,SchoolYear
+      ,[Archive]
+      ,[ArchiveDate]
+      ,[Menu_Code_Custom]
+      ,[FTBO_Custom]
+  FROM [HISDNutritionalServices].[dbo].[StudentEntryData]
+*/
+
+
+/*UPDATE StudentEntryData
+SET
+Archive = NULL,
+ArchiveDate = NULL
+*/
